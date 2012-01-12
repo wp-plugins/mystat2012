469d3bf0aac9ed1d377fea6652bf93b2312c03c1
sebbio1 <sebbio1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-12 11:17:04 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mystat2012/tags/2.3@488679 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ modules/geoip/locale.php
@@ -0,0 +1,553 @@
+<?php
+
+class Locale{
+
+    var $main=array(
+        "aa"=>array(
+            "en_language" => "Afar",
+            "native_language" => "Qafar"
+        ),
+        "af"=>array(
+            "en_language" => "Afrikaans",
+            "native_language" => "Afrikaans"
+        ),
+        "ak"=>array(
+            "en_language" => "Akan",
+            "native_language" => "Akan"
+        ),
+        "am"=>array(
+            "en_language" => "Amharic",
+            "native_language" => "አማርኛ"
+        ),
+        "ar"=>array(
+            "en_language" => "Arabic",
+            "native_language" => "العربية"
+        ),
+        "as"=>array(
+            "en_language" => "Assamese",
+            "native_language" => "অসমীয়া"
+        ),
+        "az"=>array(
+            "en_language" => "Azerbaijani",
+            "native_language" => "Азәрбајҹан"
+        ),
+        "be"=>array(
+            "en_language" => "Belarusian",
+            "native_language" => "беларуская"
+        ),
+        "bg"=>array(
+            "en_language" => "Bulgarian",
+            "native_language" => "български"
+        ),
+        "bn"=>array(
+            "en_language" => "Bengali",
+            "native_language" => "বাংলা"
+        ),
+        "bo"=>array(
+            "en_language" => "Tibetan",
+            "native_language" => "པོད་སྐད་"
+        ),
+        "bs"=>array(
+            "en_language" => "Bosnian",
+            "native_language" => "bosanski"
+        ),
+        "byn"=>array(
+            "en_language" => "Blin",
+            "native_language" => "ብሊን"
+        ),
+        "ca"=>array(
+            "en_language" => "Catalan",
+            "native_language" => "català"
+        ),
+        "cch"=>array(
+            "en_language" => "Atsam",
+            "native_language" => "Atsam"
+        ),
+        "cop"=>array(
+            "en_language" => "Coptic",
+            "native_language" => "Coptic"
+        ),
+        "cs"=>array(
+            "en_language" => "Czech",
+            "native_language" => "čeština"
+        ),
+        "cy"=>array(
+            "en_language" => "Welsh",
+            "native_language" => "Cymraeg"
+        ),
+        "da"=>array(
+            "en_language" => "Danish",
+            "native_language" => "dansk"
+        ),
+        "de"=>array(
+            "en_language" => "German",
+            "native_language" => "Deutsch"
+        ),
+        "dv"=>array(
+            "en_language" => "Divehi",
+            "native_language" => "ދިވެހިބަސް"
+        ),
+        "dz"=>array(
+            "en_language" => "Dzongkha",
+            "native_language" => "རྫོང་ཁ"
+        ),
+        "ee"=>array(
+            "en_language" => "Ewe",
+            "native_language" => "Ewe"
+        ),
+        "el"=>array(
+            "en_language" => "Greek",
+            "native_language" => "Ἑλληνικά"
+        ),
+        "en"=>array(
+            "en_language" => "English",
+            "native_language" => "English"
+        ),
+        "eo"=>array(
+            "en_language" => "Esperanto",
+            "native_language" => "esperanto"
+        ),
+        "es"=>array(
+            "en_language" => "Spanish",
+            "native_language" => "español"
+        ),
+        "et"=>array(
+            "en_language" => "Estonian",
+            "native_language" => "eesti"
+        ),
+        "eu"=>array(
+            "en_language" => "Basque",
+            "native_language" => "euskara"
+        ),
+        "fa"=>array(
+            "en_language" => "Persian",
+            "native_language" => "دری"
+        ),
+        "fi"=>array(
+            "en_language" => "Finnish",
+            "native_language" => "suomi"
+        ),
+        "fil"=>array(
+            "en_language" => "Filipino",
+            "native_language" => "Filipino"
+        ),
+        "fo"=>array(
+            "en_language" => "Faroese",
+            "native_language" => "føroyskt"
+        ),
+        "fr"=>array(
+            "en_language" => "French",
+            "native_language" => "français"
+        ),
+        "fur"=>array(
+            "en_language" => "Friulian",
+            "native_language" => "furlan"
+        ),
+        "ga"=>array(
+            "en_language" => "Irish",
+            "native_language" => "Gaeilge"
+        ),
+        "gaa"=>array(
+            "en_language" => "Ga",
+            "native_language" => "Ga"
+        ),
+        "gez"=>array(
+            "en_language" => "Geez",
+            "native_language" => "ግዕዝኛ"
+        ),
+        "gl"=>array(
+            "en_language" => "Galician",
+            "native_language" => "galego"
+        ),
+        "gu"=>array(
+            "en_language" => "Gujarati",
+            "native_language" => "ગુજરાતી"
+        ),
+        "gv"=>array(
+            "en_language" => "Manx",
+            "native_language" => "Gaelg"
+        ),
+        "ha"=>array(
+            "en_language" => "Hausa",
+            "native_language" => "Hausa"
+        ),
+        "haw"=>array(
+            "en_language" => "Hawaiian",
+            "native_language" => "ʻōlelo Hawaiʻi"
+        ),
+        "he"=>array(
+            "en_language" => "Hebrew",
+            "native_language" => "עברית"
+        ),
+        "hi"=>array(
+            "en_language" => "Hindi",
+            "native_language" => "हिंदी"
+        ),
+        "hr"=>array(
+            "en_language" => "Croatian",
+            "native_language" => "hrvatski"
+        ),
+        "hu"=>array(
+            "en_language" => "Hungarian",
+            "native_language" => "magyar"
+        ),
+        "hy"=>array(
+            "en_language" => "Armenian",
+            "native_language" => "Հայերէն"
+        ),
+        "ia"=>array(
+            "en_language" => "Interlingua",
+            "native_language" => "interlingua"
+        ),
+        "id"=>array(
+            "en_language" => "Indonesian",
+            "native_language" => "Bahasa Indonesia"
+        ),
+        "ig"=>array(
+            "en_language" => "Igbo",
+            "native_language" => "Igbo"
+        ),
+        "ii"=>array(
+            "en_language" => "Sichuan Yi",
+            "native_language" => "ꆈꌠꉙ"
+        ),
+        "is"=>array(
+            "en_language" => "Icelandic",
+            "native_language" => "íslenska"
+        ),
+        "it"=>array(
+            "en_language" => "Italian",
+            "native_language" => "italiano"
+        ),
+        "iu"=>array(
+            "en_language" => "Inuktitut",
+            "native_language" => "ᐃᓄᒃᑎᑐᑦ ᑎᑎᕋᐅᓯᖅ"
+        ),
+        "ja"=>array(
+            "en_language" => "Japanese",
+            "native_language" => "日本語"
+        ),
+        "ka"=>array(
+            "en_language" => "Georgian",
+            "native_language" => "ქართული"
+        ),
+        "kaj"=>array(
+            "en_language" => "Jju",
+            "native_language" => "Jju"
+        ),
+        "kam"=>array(
+            "en_language" => "Kamba",
+            "native_language" => "Kamba"
+        ),
+        "kcg"=>array(
+            "en_language" => "Tyap",
+            "native_language" => "Tyap"
+        ),
+        "kfo"=>array(
+            "en_language" => "Koro",
+            "native_language" => "Koro"
+        ),
+        "kk"=>array(
+            "en_language" => "Kazakh",
+            "native_language" => "Қазақ"
+        ),
+        "kl"=>array(
+            "en_language" => "Kalaallisut",
+            "native_language" => "kalaallisut"
+        ),
+        "km"=>array(
+            "en_language" => "Khmer",
+            "native_language" => "ភាសាខ្មែរ"
+        ),
+        "kn"=>array(
+            "en_language" => "Kannada",
+            "native_language" => "ಕನ್ನಡ"
+        ),
+        "ko"=>array(
+            "en_language" => "Korean",
+            "native_language" => "한국어"
+        ),
+        "kok"=>array(
+            "en_language" => "Konkani",
+            "native_language" => "कोंकणी"
+        ),
+        "kpe"=>array(
+            "en_language" => "Kpelle",
+            "native_language" => "Kpelle"
+        ),
+        "ku"=>array(
+            "en_language" => "Kurdish",
+            "native_language" => "كوردی"
+        ),
+        "kw"=>array(
+            "en_language" => "Cornish",
+            "native_language" => "kernewek"
+        ),
+        "ky"=>array(
+            "en_language" => "Kirghiz",
+            "native_language" => "Кыргыз"
+        ),
+        "ln"=>array(
+            "en_language" => "Lingala",
+            "native_language" => "lingála"
+        ),
+        "lo"=>array(
+            "en_language" => "Lao",
+            "native_language" => "ລາວ"
+        ),
+        "lt"=>array(
+            "en_language" => "Lithuanian",
+            "native_language" => "lietuvių"
+        ),
+        "lv"=>array(
+            "en_language" => "Latvian",
+            "native_language" => "latviešu"
+        ),
+        "mk"=>array(
+            "en_language" => "Macedonian",
+            "native_language" => "македонски"
+        ),
+        "ml"=>array(
+            "en_language" => "Malayalam",
+            "native_language" => "മലയാളം"
+        ),
+        "mn"=>array(
+            "en_language" => "Mongolian",
+            "native_language" => "монгол"
+        ),
+        "mr"=>array(
+            "en_language" => "Marathi",
+            "native_language" => "मराठी"
+        ),
+        "ms"=>array(
+            "en_language" => "Malay",
+            "native_language" => "Bahasa Melayu"
+        ),
+        "mt"=>array(
+            "en_language" => "Maltese",
+            "native_language" => "Malti"
+        ),
+        "my"=>array(
+            "en_language" => "Burmese",
+            "native_language" => "Burmese"
+        ),
+        "nb"=>array(
+            "en_language" => "Norwegian Bokmål",
+            "native_language" => "norsk bokmål"
+        ),
+        "ne"=>array(
+            "en_language" => "Nepali",
+            "native_language" => "नेपाली"
+        ),
+        "nl"=>array(
+            "en_language" => "Dutch",
+            "native_language" => "Nederlands"
+        ),
+        "nn"=>array(
+            "en_language" => "Norwegian Nynorsk",
+            "native_language" => "nynorsk"
+        ),
+        "nr"=>array(
+            "en_language" => "South Ndebele",
+            "native_language" => "isiNdebele"
+        ),
+        "nso"=>array(
+            "en_language" => "Northern Sotho",
+            "native_language" => "Sesotho sa Leboa"
+        ),
+        "ny"=>array(
+            "en_language" => "Nyanja",
+            "native_language" => "Nyanja"
+        ),
+        "om"=>array(
+            "en_language" => "Oromo",
+            "native_language" => "Oromoo"
+        ),
+        "or"=>array(
+            "en_language" => "Oriya",
+            "native_language" => "ଓଡ଼ିଆ"
+        ),
+        "pa"=>array(
+            "en_language" => "Punjabi",
+            "native_language" => "پنجاب"
+        ),
+        "pl"=>array(
+            "en_language" => "Polish",
+            "native_language" => "polski"
+        ),
+        "ps"=>array(
+            "en_language" => "Pashto",
+            "native_language" => "پښتو"
+        ),
+        "pt"=>array(
+            "en_language" => "Portuguese",
+            "native_language" => "português"
+        ),
+        "ro"=>array(
+            "en_language" => "Romanian",
+            "native_language" => "română"
+        ),
+        "ru"=>array(
+            "en_language" => "Russian",
+            "native_language" => "русский"
+        ),
+        "rw"=>array(
+            "en_language" => "Kinyarwanda",
+            "native_language" => "Kinyarwanda"
+        ),
+        "sa"=>array(
+            "en_language" => "Sanskrit",
+            "native_language" => "संस्कृत भाषा"
+        ),
+        "se"=>array(
+            "en_language" => "Northern Sami",
+            "native_language" => "se"
+        ),
+        "sh"=>array(
+            "en_language" => "Serbo\-Croatian",
+            "native_language" => "Serbo\-Croatian"
+        ),
+       "sid"=>array(
+            "en_language" => "Sidamo",
+            "native_language" => "Sidaamu Afo"
+        ),
+        "sk"=>array(
+            "en_language" => "Slovak",
+            "native_language" => "slovenský"
+        ),
+        "sl"=>array(
+            "en_language" => "Slovenian",
+            "native_language" => "slovenščina"
+        ),
+        "so"=>array(
+            "en_language" => "Somali",
+            "native_language" => "Soomaali"
+        ),
+        "sq"=>array(
+            "en_language" => "Albanian",
+            "native_language" => "shqipe"
+        ),
+        "sr"=>array(
+            "en_language" => "Serbian",
+            "native_language" => "Srpski"
+        ),
+        "ss"=>array(
+            "en_language" => "Swati",
+            "native_language" => "Siswati"
+        ),
+        "st"=>array(
+            "en_language" => "Southern Sotho",
+            "native_language" => "Sesotho"
+        ),
+        "sv"=>array(
+            "en_language" => "Swedish",
+            "native_language" => "svenska"
+        ),
+        "sw"=>array(
+            "en_language" => "Swahili",
+            "native_language" => "Kiswahili"
+        ),
+        "syr"=>array(
+            "en_language" => "Syriac",
+            "native_language" => "ܣܘܪܝܝܐ"
+        ),
+        "ta"=>array(
+            "en_language" => "Tamil",
+            "native_language" => "தமிழ்"
+        ),
+        "te"=>array(
+            "en_language" => "Telugu",
+            "native_language" => "తెలుగు"
+        ),
+        "tg"=>array(
+            "en_language" => "Tajik",
+            "native_language" => "Tajik"
+        ),
+        "th"=>array(
+            "en_language" => "Thai",
+            "native_language" => "ไทย"
+        ),
+        "ti"=>array(
+            "en_language" => "Tigrinya",
+            "native_language" => "ትግርኛ"
+        ),
+        "tig"=>array(
+            "en_language" => "Tigre",
+            "native_language" => "ትግረ"
+        ),
+        "tn"=>array(
+            "en_language" => "Tswana",
+            "native_language" => "Setswana"
+        ),
+        "to"=>array(
+            "en_language" => "Tonga",
+            "native_language" => "lea fakatonga"
+        ),
+        "tr"=>array(
+            "en_language" => "Turkish",
+            "native_language" => "Türkçe"
+        ),
+        "ts"=>array(
+            "en_language" => "Tsonga",
+            "native_language" => "Xitsonga"
+        ),
+        "tt"=>array(
+            "en_language" => "Tatar",
+            "native_language" => "Татар"
+        ),
+        "ug"=>array(
+            "en_language" => "Uighur",
+            "native_language" => "Uighur"
+        ),
+        "uk"=>array(
+            "en_language" => "Ukrainian",
+            "native_language" => "українська"
+        ),
+        "ur"=>array(
+            "en_language" => "Urdu",
+            "native_language" => "اردو"
+        ),
+        "uz"=>array(
+            "en_language" => "Uzbek",
+            "native_language" => "o\'zbekcha"
+        ),
+        "ve"=>array(
+            "en_language" => "Venda",
+            "native_language" => "Tshivenḓa"
+        ),
+        "vi"=>array(
+            "en_language" => "Vietnamese",
+            "native_language" => "Tiếng Việt"
+        ),
+        "wal"=>array(
+            "en_language" => "Walamo",
+            "native_language" => "ወላይታቱ"
+        ),
+        "wo"=>array(
+            "en_language" => "Wolof",
+            "native_language" => "Wolof"
+        ),
+        "xh"=>array(
+            "en_language" => "Xhosa",
+            "native_language" => "isiXhosa"
+        ),
+        "yo"=>array(
+            "en_language" => "Yoruba",
+            "native_language" => "Yoruba"
+        ),
+        "zh"=>array(
+            "en_language" => "Chinese",
+            "native_language" => "中文"
+        ),
+        "zu"=>array(
+            "en_language" => "Zulu",
+            "native_language" => "isiZulu"
+        )
+    );
+
+    function get_locale($name){
+        if($name==''){return false;};
+        return $this->main[$name];
+    }
+};
+?>
